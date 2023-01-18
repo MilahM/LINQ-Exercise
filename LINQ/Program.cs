b62a6007fe5a486e4c8020ac75231bd1374a9942
Kamilah <k.muhammad95@gmail.com>
2023-01-18 07:53:55 -0600

    linq complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Collections.Immutable;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameNames = new List<string>() { "COD", "NBA 2k", "Fortnite", "Madden", "GTA", "Mortal Kombat" };
+
+           var names = gameNames.OrderBy(x => x.Length);
+
+            foreach (var n in names)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
